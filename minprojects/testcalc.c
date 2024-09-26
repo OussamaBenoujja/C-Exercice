297f1cfae094a71aee2814046d61019144fe7c14
OussamaBenoujja <osama2code79@gmail.com>
2024-09-26 11:01:00 +0100

    Delete minprojects/testcalc.c

--- minprojects/testcalc.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-
-int main(){
-    printf("\n1.Addition : Ajouter deux ou plusieurs nombres.\n2.Soustraction : Soustraire deux nombres.\n3.Multiplication : Multiplier deux ou plusieurs nombres.\n4.Division : Diviser deux nombres, avec gestion de la division par zéro.\n5.Moyenne : Calculer la moyenne d une série de nombres.\n6.Valeur absolue : Calculer la valeur absolue d un nombre.\n7.Exponentiation : Calculer un nombre à une certaine puissance.\n8.Racine carrée : Calculer la racine carrée d un nombre positif.");
-}
