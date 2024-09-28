8ead92391bb6c1c1f20d844bd357cbd5bc744427
OussamaBenoujja <osama2code79@gmail.com>
2024-09-28 10:44:44 +0100

    commit saturday

--- minprojects/gestionStockLibrairie.c
+++ minprojects/gestionStockLibrairie.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
 #define max 100
 
@@ -13,40 +14,47 @@ int main(){
         int count = 0;
         int choice;
         char rchstring[max];
+        bool isNum;
+        bool quit = false;
 
         while(true){
-        printf("\n--- Système de Gestion de Stock ---\n");
+        printf("\n--- Systeme de Gestion de Stock ---\n");
         printf("1. Ajouter un livre au stock\n");
         printf("2. Afficher tous les livres disponibles\n");
         printf("3. Rechercher un livre par son titre\n");
-        printf("4. Mettre à jour la quantité d'un livre\n");
+        printf("4. Mettre a jour la quantite d'un livre\n");
         printf("5. Supprimer un livre du stock\n");
         printf("6. Afficher le nombre total de livres en stock\n");
         printf("7. Quitter\n");
         printf("Choisissez une option : ");
+        scanf("\n");
         scanf("%d", &choice);
 
         switch (choice){
         case 1:
                 printf("\nentrez nom de livre : ");
-                scanf("%s", titles[count]);
+                scanf("\n");
+                fgets(titles[count], max, stdin);
                 printf("\nentrez nom de author : ");
-                scanf("%s", authors[count]);
+                scanf("\n");
+                fgets(authors[count], max, stdin);
                 printf("\nentrez prix : ");
-                scanf("%f", &prices[count]);
+                scanf("\n");
+                isNum = scanf("%f", &prices[count]);
                 printf("\nentrez quantity : ");
-                scanf("%d", &quantities[count]);
+                scanf("\n");
+                isNum = scanf("%d", &quantities[count]);
                 count++;
-            break;
+                break;
         case 2:
-            for(int i=0;i<=count;i++){
+            for(int i=0;i<count;i++){
                 printf("\ntitre : %s author : %s prix : %.2f quantity : %d",titles[i],authors[i],prices[i],quantities[i]);
             }
             break;
         case 3:
-            
                 printf("\nentrez titre du livre : ");
-                scanf("%s", rchstring);
+                scanf("\n");
+                fgets(rchstring, max, stdin);
                 bool found = false;
                 for (int i = 0; i < count; i++) {
                     if (strcmp(titles[i], rchstring) == 0) {
@@ -58,5 +66,53 @@ int main(){
                     printf("\nlivre pas disponible\n");
                 }
                 break;
-        }}
+        case 4:
+                printf("\nentrez titre du livre : ");
+                scanf("\n");
+                fgets(rchstring, max, stdin);
+                bool found0 = false;
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(titles[i], rchstring) == 0) {
+                        printf("\nEntrez novelle Quantity :");
+                        scanf("%d", &quantities[i]);
+                        found0 = true;
+                    }
+                }
+                if (!found0) {
+                    printf("\nlivre pas disponible\n");
+                }
+                break;
+        case 5:
+                printf("\nentrez titre du livre : ");
+                scanf("\n");
+                fgets(rchstring, max, stdin);
+                bool found1 = false;
+                for (int i = 0; i < count; i++) {
+                    if (strcmp(titles[i], rchstring) == 0) {
+                        found1 = true;
+                    }
+                    if(found1){
+                            for(int j=0;j<max;j++){
+                            titles[i][j] = titles[i+1][j];
+                            authors[i][j] = authors[i+1][j];
+                            prices[i] = prices[i+1];
+                            quantities[i] = quantities[i+1];
+                        }count--;
+                    }
+                }
+                if (!found1) {
+                    printf("\nlivre pas disponible\n");
+                }
+                break;
+        case 6:
+            printf("\nnombre de livre est %d",count);
+            break;
+        case 7:
+            quit =true;
+            break;
+        }
+        if(quit){
+            break;
+        }
+        }
 }
