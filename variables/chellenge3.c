b282bfc2a41b0c09935d80ab9da4daa12d592b31
Oussama Benoujja <osama2code79@gmail.com>
2024-09-28 16:01:08 +0100

    Delete variables directory

--- variables/chellenge3.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    float distance;
-    printf("distance en KM : ");
-    scanf("%f", &distance);
-    printf("\ndistance en yards: %.2f", distance * 1093.61);
-}
