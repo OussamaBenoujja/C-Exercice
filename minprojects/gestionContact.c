259261f6e58c0a35d08017a7f5a3972088e7363d
OussamaBenoujja <osama2code79@gmail.com>
2024-09-30 17:02:36 +0100

    commit minproj

--- minprojects/gestionContact.c
+++ minprojects/gestionContact.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define max 100
 
@@ -9,23 +10,61 @@ typedef struct contact{
     char email[max];
 }contact;
 
+contact contacts[max];
+
 
 void add(contact *c){
     char em1[max];
     char em2[max];
     char em3[max];
     printf("\nEntrez un nom : ");
-    fget(c->nom,sizeof(c->nom),stdin);
+    fget(*c->nom,sizeof(*c->nom),stdin);
     printf("\nEntrez un Numero de téléphone : ");
-    scanf("%s", c->tel);
+    scanf("%s", *c->tel);
     printf("\nEntrez un Adresse e-mail : ");
     scanf("%s@%s.%s", em1,em2,em3);
-    
+    strcat(em1,"@");
+    strcat(em2,".");
+    strcat(em1,em2);
+    strcat(em1,em3);
+    strcpy(*c->email,em1);
+}
+void update(contact *c[max],char name[max]){
+    bool found = false;
+    char em1[max];
+    char em2[max];
+    char em3[max];
+    for(int i=0;i<max;i++){
+        if(strcmp(c[i]->nom,name)==0){
+            found = true;
+            printf("\nEntrez un Numero de téléphone : ");
+            scanf("%s", *c[i]->tel);
+            printf("\nEntrez un Adresse e-mail : ");
+            scanf("%s@%s.%s", em1,em2,em3);
+            strcat(em1,"@");
+            strcat(em2,".");
+            strcat(em1,em2);
+            strcat(em2,em3);
+            strcpy(*c[i]->email,em1);
+        }
+    }if(!found){printf("\ncontact not found !");}
+}
+void delete(contact *c[max],char name[max]){
+    bool found = false;
+    for(int i=0;i<max;i++){
+        if(strcmp(*c[i]->nom,name)==0){
+            found = true;
+            *c[i]->nom = *c[i+1]->nom;
+            *c[i]->tel = *c[i+1]->tel;
+            *c[i]->email = *c[i+1]->email;
+            }
+    }if(!found){printf("\ncontact not found !");}
 }
 
 
+
 int main(){
+    while(true){
 
-
-
+    }
 }
